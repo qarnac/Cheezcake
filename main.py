592d17fc63a2eaa30ea4fc2bdd62bcbe23a080c8
Adrian <adresubal@gmail.com>
2018-08-13 14:01:27 -0700

    added initial files

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import jinja2
+import webapp2
+
+env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class HomePage(webapp2.RequestHandler):
+    def get(self):
+        mypage = env.get_template('templates/home.html')
+        self.response.write(mypage.render())
+
+app =   webapp2.WSGIApplication([
+    ('/', HomePage),
+], debug=True)
