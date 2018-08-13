f027eaac851a22b8884de6e49f4b49e809ab0282
Adrian <adresubal@gmail.com>
2018-08-13 16:16:07 -0700

    main py api changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import jinja2
 import webapp2
+from pytrends.request import TrendReq
+from datetime import datetime
 
 env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -9,6 +11,10 @@ env = jinja2.Environment(
 
 class HomePage(webapp2.RequestHandler):
     def get(self):
+        #
+        # pytrends = TrendReq(hl='en-US', tz=360)
+        # musictable = pytrends.top_charts(yyyymm, 'music', geo='us', cat='')
+
         mypage = env.get_template('templates/home.html')
         self.response.write(mypage.render())
 
