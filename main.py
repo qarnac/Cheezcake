b5285b3ea3d7e938f661bbd5f407a965d9e46832
Veronica Ung <ung003@csusm.edu>
2018-08-14 15:07:45 -0700

    Connect the about page to local host

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ env = jinja2.Environment(
 
 class HomePage(webapp2.RequestHandler):
     def get(self):
-
         mypage = env.get_template('templates/home.html')
+        self.response.write(mypage.render())
+
+class AboutPage(webapp2.RequestHandler):
+    def get(self):
+        mypage = env.get_template('templates/about.html')
         self.response.write(mypage.render())
 
 app =   webapp2.WSGIApplication([
     ('/', HomePage),
+    ('/aboutus', AboutPage)
 ], debug=True)
- 
