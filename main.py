8e553e8401439fb242b7abc50614fdcbf6f5b3ab
Adrian <adresubal@gmail.com>
2018-08-16 15:14:12 -0700

    music data load

--- main.py
+++ main.py
@@ -23,14 +23,26 @@ class AboutPage(webapp2.RequestHandler):
 class MusicPage(webapp2.RequestHandler):
     def get(self):
         mypage = env.get_template('templates/music.html')
+
         q = DataItem.query(DataItem.category == 'music')
         q = q.order(DataItem.ranking)
-
         string = []
         for p in q.fetch():
             string.append((str(p.ranking) + ": " + p.title + " by " + p.info))
 
-        self.response.out.write(mypage.render({'elements1': string}))
+        q2 = DataItem.query(DataItem.category == 'music-us')
+        q2 = q2.order(DataItem.ranking)
+        string2 = []
+        for p in q2.fetch():
+            string2.append((str(p.ranking) + ": " + p.title + " by " + p.info))
+
+        q3 = DataItem.query(DataItem.category == 'music-artists')
+        q3 = q3.order(DataItem.ranking)
+        string3 = []
+        for p in q3.fetch():
+            string3.append((str(p.ranking) + ": " + p.title))
+
+        self.response.out.write(mypage.render({'elements1': string, 'elements2': string2, 'elements3':string3}))
 
 class MoviePage(webapp2.RequestHandler):
     def get(self):
