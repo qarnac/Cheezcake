a1382fff9af554552bcab837aa5ccf5195cdbaca
Adrian <adresubal@gmail.com>
2018-08-16 20:29:34 -0700

    formatting of data in website

--- main.py
+++ main.py
@@ -42,22 +42,84 @@ class MusicPage(webapp2.RequestHandler):
         for p in q3.fetch(10):
             string3.append((str(p.ranking) + ": " + p.title))
 
-        self.response.out.write(mypage.render({'elements1': string, 'elements2': string2, 'elements3':string3}))
+        self.response.out.write(mypage.render({'elements1': string,
+                                               'elements2': string2, 'elements3':string3}))
 
 class MoviePage(webapp2.RequestHandler):
     def get(self):
         mypage = env.get_template('templates/movie.html')
-        self.response.write(mypage.render())
+
+        q = DataItem.query(DataItem.category == 'movies')
+        q = q.order(DataItem.ranking)
+        string = []
+
+        for p in q.fetch(10):
+            string.append((str(p.ranking) + ": " + p.title))
+
+        q2 = DataItem.query(DataItem.category == 'movies-us')
+        q2 = q2.order(DataItem.ranking)
+        string2 = []
+        for p in q2.fetch(10):
+            string2.append((str(p.ranking) + ": " + p.title))
+
+        q3 = DataItem.query(DataItem.category == 'upcoming')
+        q3 = q3 .order(DataItem.ranking)
+        string3 = []
+        for p in q3.fetch(10):
+            string3.append((str(p.ranking) + ": "+ p.title))
+
+        self.response.out.write(mypage.render({'elements1': string,
+                                               'elements2': string2, 'elements3':string3}))
 
 class GamePage(webapp2.RequestHandler):
     def get(self):
         mypage = env.get_template('templates/game.html')
-        self.response.write(mypage.render())
+
+        q = DataItem.query(DataItem.category == 'top-games')
+        q = q.order(DataItem.ranking)
+        string = []
+        for p in q.fetch(10):
+            string.append((str(p.ranking) + ": " + p.title))
+
+        q2 = DataItem.query(DataItem.category == 'trending-games')
+        q2 = q2.order(DataItem.ranking)
+        string2 = []
+        for p in q2.fetch(10):
+            string2.append((str(p.ranking) + ": " + p.title))
+
+        q3 = DataItem.query(DataItem.category == 'top-board')
+        q3 = q3 .order(DataItem.ranking)
+        string3 = []
+        for p in q3.fetch(10):
+            string3.append((str(p.ranking) + ": " + p.title))
+
+        self.response.out.write(mypage.render({'elements1': string,
+                                               'elements2': string2, 'elements3':string3}))
 
 class BookPage(webapp2.RequestHandler):
     def get(self):
         mypage = env.get_template('templates/book.html')
-        self.response.write(mypage.render())
+
+        q = DataItem.query(DataItem.category == 'top-books')
+        q = q.order(DataItem.ranking)
+        string = []
+        for p in q.fetch(10):
+            string.append((str(p.ranking) + ": " + p.title + " by " + p.info))
+
+        q2 = DataItem.query(DataItem.category == 'top-us')
+        q2 = q2.order(DataItem.ranking)
+        string2 = []
+        for p in q2.fetch(10):
+            string2.append((str(p.ranking) + ": " + p.title + " by " + p.info))
+
+        q3 = DataItem.query(DataItem.category == 'authors')
+        q3 = q3 .order(DataItem.ranking)
+        string3 = []
+        for p in q3.fetch(10):
+            string3.append((str(p.ranking) + ": " + p.title))
+
+        self.response.out.write(mypage.render({'elements1': string,
+                                               'elements2': string2, 'elements3':string3}))
 
 class dataDev(webapp2.RequestHandler):
     def get(self):
@@ -71,7 +133,8 @@ class dataDev(webapp2.RequestHandler):
         info = self.request.get('info')
         imgsrc = self.request.get('imgsrc')
 
-        newItem = DataItem(category=cat, ranking=ranking, title=title, info=info, imgsrc=imgsrc)
+        newItem = DataItem(category=cat, ranking=ranking, title=title,
+                           info=info, imgsrc=imgsrc)
         newItem.put()
 
         mypage = env.get_template('templates/datadev.html')
